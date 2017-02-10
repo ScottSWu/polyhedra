76d47e819dc1d47a5ce5fcb77a4009ee59432deb
Daniel Weber <dantech88@yahoo.com>
2017-02-10 17:54:09 -0500

    added strawberry

--- src/fruit_test.cpp
+++ src/fruit_test.cpp
@@ -7,15 +7,17 @@
 
 #include "fruit.h"
 #include "watermelon.h"
+#include "strawberry.h"
 
 // Enables us to use cout and endl without prepending std::
 using namespace std;
 
 int main() {
     std::vector<std::shared_ptr<Fruit>> fruits;
- 
+
     // Add your name and fruit  here!
     fruits.push_back(std::make_shared<Watermelon>("Trevor Edwards"));
+    fruits.push_back(std::make_shared<Strawberry>("Me"));
     // Don't edit below
     std::srand(1996);
     for (auto itr = fruits.begin(); itr != fruits.end(); itr++) {
@@ -39,4 +41,3 @@ int main() {
     printf("All fruits are delicious!\n");
     return 0;
 }
-
